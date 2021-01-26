108434cb2fd04cd2828035d6beaf8e619e7036c5
ProbablyAimee <maevrim@gmail.com>
2021-01-25 22:37:27 -0500

    initial

---
+++ NMap/playersData.js
@@ -0,0 +1,177 @@
+// NOTE: Please only modify player attributes below such as name, color, and visible
+// To hide a player's marker, change their 'visible' variable from 'true' to 'false'
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Changes will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var playersData = {
+ "players": [
+    {
+      "uuid": " ",
+      "name": "Nether Fortress Base",
+      "dimensionId": 1,
+      "position": [
+        -212,
+        67,
+        29
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Basalt Delta Overlook",
+      "dimensionId": 1,
+      "position": [
+        45,
+        46,
+        176
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Basalt Quarry",
+      "dimensionId": 1,
+      "position": [
+        48,
+        43,
+        -24
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Base Portal",
+      "dimensionId": 1,
+      "position": [
+        7,
+        44,
+        -1
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Crimson Forest Tunnel",
+      "dimensionId": 1,
+      "position": [
+        -106,
+        52,
+        187
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Diorite Outlet",
+      "dimensionId": 1,
+      "position": [
+        -78,
+        37,
+        -66
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Lava Bridge 1",
+      "dimensionId": 1,
+      "position": [
+        -62,
+        33,
+        -60
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Long Tunnel Crossroad",
+      "dimensionId": 1,
+      "position": [
+        -44,
+        51,
+        187
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Magma Ports Room",
+      "dimensionId": 1,
+      "position": [
+        -117,
+        33,
+        37
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Portal 2",
+      "dimensionId": 1,
+      "position": [
+        31,
+        43,
+        -10
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Soul Valley Exit",
+      "dimensionId": 1,
+      "position": [
+        -36,
+        43,
+        304
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Bastion Stable",
+      "dimensionId": 1,
+      "position": [
+        -530,
+        215,
+        52
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Bastard Bastion",
+      "dimensionId": 1,
+      "position": [
+        -555,
+        50,
+        181
+      ],
+      "color": "Green",
+      "visible": true
+    },
+    {
+      "uuid": " ",
+      "name": "Tunnel Start",
+      "dimensionId": 1,
+      "position": [
+        9,
+        39,
+        43
+      ],
+      "color": "Green",
+      "visible": true
+    }
+  ]
+};
+
