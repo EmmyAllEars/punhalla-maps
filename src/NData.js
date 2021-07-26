19aab7ada239712ddee58b7536d5bca2e960dcc7
Emelin Ringuette <maevrim@gmail.com>
2021-07-26 12:47:51 -0400

    moved folders

--- src/NData.js
+++ src/NData.js
@@ -3,9 +3,10 @@ var playersData = {
  // lighthouse/torch markers - Yellow
  // Fire icon - \n\uf7e4
  
-//Bases - Green
+//-------------------------Bases - Green---------------------------
 //Home icon \n\uf015
-    {
+
+/*    {
       "uuid": " ",
       "name": "Nether Fortress Base #2\n\uf015",
       "dimensionId": 1,
@@ -16,22 +17,12 @@ var playersData = {
       ],
       "color": "Green",
       "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Nether Fortress Base\n\uf015",
-      "dimensionId": 1,
-      "position": [
-        -212,
-        67,
-        29
-      ],
-      "color": "Green",
-      "visible": true
-    },
-//Nether Portals -- Purple
+    },*/
+
+//------------------------Nether Portals -- Purple-------------------
 // archway icon \n\uf557
-    {
+
+ /*   {
       "uuid": " ",
       "name": "Mountain Portal\n\uf557",
       "dimensionId": 1,
@@ -42,70 +33,11 @@ var playersData = {
       ],
       "color": "Purple",
       "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Base Portal\n\uf557",
-      "dimensionId": 1,
-      "position": [
-        7,
-        44,
-        -1
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Portal 2\n\uf557",
-      "dimensionId": 1,
-      "position": [
-        31,
-        43,
-        -10
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Stronghold Portal\n\uf557",
-      "dimensionId": 1,
-      "position": [
-        -102,
-        14,
-        -83
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Old Village Nether Portal\n\uf557",
-      "dimensionId": 1,
-      "position": [
-        -27,
-        13,
-        -43
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Desert Portal\n\uf557",
-      "dimensionId": 0,
-      "position": [
-        -20,
-        0,
-        306
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-//Tunnel Locations -- Gray
+    }, */
+    
+//--------------------------Tunnel Locations -- Gray------------------------
 // \n\uf6d9
-{
+/*{
       "uuid": " ",
       "name": "Tunnel to Y14\n\uf6d9",
       "dimensionId": 1,
@@ -116,94 +48,12 @@ var playersData = {
       ],
       "color": "Gray",
       "visible": true
-    },
-{
-      "uuid": " ",
-      "name": "Crimson Forest Tunnel\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -106,
-        52,
-        187
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Strider Baby Exit\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -117,
-        32,
-        12
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Lava Bridge 1\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -62,
-        33,
-        -60
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Long Tunnel Crossroad\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -44,
-        51,
-        187
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Magma Ports Room\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -117,
-        33,
-        37
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-     {
-      "uuid": " ",
-      "name": "Soul Valley Exit\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -36,
-        43,
-        304
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Tunnel Start\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        9,
-        39,
-        43
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-//Landmarks -- DarkGreen
+    }, */
+
+//-------------------------Landmarks -- DarkGreen----------------------------
 // \n\uf5a6
-    {
+ 
+ /*   {
       "uuid": " ",
       "name": "Basalt Delta Overlook\n\uf5a6",
       "dimensionId": 1,
@@ -214,55 +64,8 @@ var playersData = {
       ],
       "color": "DarkGreen",
       "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Basalt Quarry\n\uf5a6",
-      "dimensionId": 1,
-      "position": [
-        48,
-        43,
-        -24
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Diorite Outlet\n\uf5a6",
-      "dimensionId": 1,
-      "position": [
-        -78,
-        37,
-        -66
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Bastion Stable\n\uf5a6",
-      "dimensionId": 1,
-      "position": [
-        -530,
-        215,
-        52
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Bastard Bastion\n\uf5a6",
-      "dimensionId": 1,
-      "position": [
-        -555,
-        50,
-        181
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    }
+    } */
+ 
   ]
 };
 
--- src/OWData.js
+++ src/OWData.js
@@ -1,19 +1,9 @@
 var playersData = {
   "players": [
-   // lighthouse/torch markers - Orange
- // Fire icon - \n\uf7e4
- {
-      "uuid": " ",
-      "name": "\uf7e4",
-      "dimensionId": 0,
-      "position": [
-        -434,
-        0,
-        220
-      ],
-      "color": "Orange",
-      "visible": true
-    },
+// -------------------------lighthouse/torch markers - Orange-------------------------
+// Fire icon - \n\uf7e4
+
+/*
  {
       "uuid": " ",
       "name": "\uf7e4",
@@ -25,104 +15,11 @@ var playersData = {
       ],
       "color": "Orange",
       "visible": true
-    },
-//Bases - DarkRed
+    }, */
+
+//-------------------------Bases - DarkRed-------------------------
 // Home icon: \n\uf015
-{
-      "uuid": " ",
-      "name": "Shipwreck\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -759,
-        0,
-        377
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },{
-      "uuid": " ",
-      "name": "Base of Afe\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -3359,
-        0,
-        3905
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },
-  {
-      "uuid": " ",
-      "name": "Birch Hut\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        671,
-        0,
-        -930
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },
-  {
-      "uuid": " ",
-      "name": "Main Base\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        101,
-        74,
-        -70
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Emmy Witch Hut\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -812,
-        0,
-        -1820
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },
-     {
-      "uuid": " ",
-      "name": "Afe's Village\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -3434,
-        0,
-        3519
-      ],
-      "color": "DarkRed",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Abandoned Church\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -133,
-        69,
-        -339
-      ],
-      "color": "DarkRed",
-      "visible": true
-    }, 
-    {
-      "uuid": " ",
-      "name": "Pumpkin Head House\n\uf015",
-      "dimensionId": 0,
-      "position": [
-        -820,
-        0,
-        -668
-      ],
-      "color": "DarkRed",
-      "visible": true
-    }, 
+/* 
     {
       "uuid": " ",
       "name": "Mountain Base\n\uf015",
@@ -134,10 +31,12 @@ var playersData = {
       ],
       "color": "DarkRed",
       "visible": true
-    }, 
-//Nether Portals -- Purple
+    }, */
+
+//-------------------------Nether Portals -- Purple-------------------------
 // archway icon \n\uf557
-    {
+    
+    /*{
       "uuid": " ",
       "name": "Mountain Portal\n\uf557",
       "dimensionId": 0,
@@ -148,70 +47,12 @@ var playersData = {
       ],
       "color": "Purple",
       "visible": true
-    },
-  {
-      "uuid": " ",
-      "name": "Stronghold Portal\n\uf557",
-      "dimensionId": 0,
-      "position": [
-        -812,
-        0,
-        -661
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Desert Portal\n\uf557",
-      "dimensionId": 0,
-      "position": [
-        -244,
-        69,
-        2478
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Portal 2 Cave\n\uf557",
-      "dimensionId": 0,
-      "position": [
-        252,
-        41,
-        -82
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Old Village Nether Portal\n\uf557",
-      "dimensionId": 0,
-      "position": [
-        -216,
-        69,
-        -343
-      ],
-      "color": "Purple",
-      "visible": true
-    },
-//Underground Locations -- Gray
+    }, */
+ 
+//-------------------------Underground Locations -- Gray-------------------------
 // \n\uf6d9
-    {
-      "uuid": " ",
-      "name": "Fossils\n\uf6d9",
-      "dimensionId": 0,
-      "position": [
-        -72,
-        0,
-        -184
-      ],
-      "color": "Gray",
-      "visible": true
-    },
-    {
+
+/*    {
       "uuid": " ",
       "name": "Mineshaft\n\uf6d9",
       "dimensionId": 0,
@@ -222,106 +63,12 @@ var playersData = {
       ],
       "color": "Gray",
       "visible": true
-    },
-//Landmarks -- DarkGreen
+    }, */
+
+//-------------------------Landmarks -- DarkGreen-------------------------
 // \n\uf5a6
-  {
-      "uuid": " ",
-      "name": "Broken Portal\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -445,
-        64,
-        -481
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Cleared Temple\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -659,
-        71,
-        2170
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Cleared Temple\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -482,
-        65,
-        2089
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "First Desert Village\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -684,
-        68,
-        2384
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Raider Tower\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        551,
-        0,
-        -711
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Stronghold Village\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -804,
-        0,
-        -692
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "First Shipwreck\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -597,
-        63,
-        175
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
-      "uuid": " ",
-      "name": "Island Village\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -1980,
-        62,
-        2372
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    },
-    {
+
+/*    {
       "uuid": " ",
       "name": "Bamboo Forest\n\uf5a6",
       "dimensionId": 0,
@@ -332,7 +79,7 @@ var playersData = {
       ],
       "color": "DarkGreen",
       "visible": true
-    }
+    }*/
   ]
 };
 
