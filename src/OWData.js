b0ca80c087de3824190bdbd58a0a6ba71c5d4002
Emelin Ringuette <maevrim@gmail.com>
2021-07-28 12:43:47 -0400

    add multi-dim marker support - credit hrmorley34

--- src/OWData.js
+++ src/OWData.js
@@ -90,7 +90,73 @@ var playersData = {
       ],
       "color": "DarkGreen",
       "visible": true
-    }*/
+    }, */
+     // lighthouse/torch markers - Yellow
+ // Fire icon - \n\uf7e4
+ 
+//-------------------------Bases - Green---------------------------
+//Home icon \n\uf015
+
+{
+      "uuid": " ",
+      "name": "Test Nether Marker\n\uf015",
+      "dimensionId": 1,
+      "position": [
+        -128,
+        53,
+        92
+      ],
+      "color": "Green",
+      "visible": true
+    },
+
+//------------------------Nether Portals -- Purple-------------------
+// archway icon \n\uf557
+
+ /*   {
+      "uuid": " ",
+      "name": "Mountain Portal\n\uf557",
+      "dimensionId": 1,
+      "position": [
+        -188,
+        14,
+        425
+      ],
+      "color": "Purple",
+      "visible": true
+    }, */
+    
+//--------------------------Tunnel Locations -- Gray------------------------
+// \n\uf6d9
+/*{
+      "uuid": " ",
+      "name": "Tunnel to Y14\n\uf6d9",
+      "dimensionId": 1,
+      "position": [
+        -20,
+        33,
+        -38
+      ],
+      "color": "Gray",
+      "visible": true
+    }, */
+
+//-------------------------Landmarks -- DarkGreen----------------------------
+// \n\uf5a6
+ 
+ /*   {
+      "uuid": " ",
+      "name": "Basalt Delta Overlook\n\uf5a6",
+      "dimensionId": 1,
+      "position": [
+        45,
+        46,
+        176
+      ],
+      "color": "DarkGreen",
+      "visible": true
+    } */
+ 
   ]
 };
 
