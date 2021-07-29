df7beab61f3b4aa1311d0c1544c57b6d994f63f8
Emelin Ringuette <maevrim@gmail.com>
2021-07-29 16:25:10 -0400

    added new bases

--- src/OWData.js
+++ src/OWData.js
@@ -33,16 +33,28 @@ var playersData = {
     }, 
     {
       "uuid": " ",
-      "name": "Emmy's Ice Palace\n\uf015",
+      "name": "Emmy's Fortress\n\uf015",
       "dimensionId": 0,
       "position": [
-        -267,
+        -316,
         67,
-        644        
+        585        
       ],
       "color": "DarkRed",
       "visible": true
-    }, 
+    },
+    {
+      "uuid": " ",
+      "name": "MrMahoo's Jungle Kingdom\n\uf015",
+      "dimensionId": 0,
+      "position": [
+        191,
+        67,
+        -217        
+      ],
+      "color": "DarkRed",
+      "visible": true
+    },
 
 //-------------------------Nether Portals -- Purple-------------------------
 // archway icon \n\uf557
@@ -79,84 +91,18 @@ var playersData = {
 //-------------------------Landmarks -- DarkGreen-------------------------
 // \n\uf5a6
 
-/*    {
-      "uuid": " ",
-      "name": "Bamboo Forest\n\uf5a6",
-      "dimensionId": 0,
-      "position": [
-        -373,
-        63,
-        3779
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    }, */
-     // lighthouse/torch markers - Yellow
- // Fire icon - \n\uf7e4
- 
-//-------------------------Bases - Green---------------------------
-//Home icon \n\uf015
-
 {
-      "uuid": " ",
-      "name": "Test Nether Marker\n\uf015",
-      "dimensionId": 1,
-      "position": [
-        -128,
-        53,
-        92
-      ],
-      "color": "Green",
-      "visible": true
-    },
-
-//------------------------Nether Portals -- Purple-------------------
-// archway icon \n\uf557
-
- /*   {
-      "uuid": " ",
-      "name": "Mountain Portal\n\uf557",
-      "dimensionId": 1,
-      "position": [
-        -188,
-        14,
-        425
-      ],
-      "color": "Purple",
-      "visible": true
-    }, */
-    
-//--------------------------Tunnel Locations -- Gray------------------------
-// \n\uf6d9
-/*{
-      "uuid": " ",
-      "name": "Tunnel to Y14\n\uf6d9",
-      "dimensionId": 1,
-      "position": [
-        -20,
-        33,
-        -38
-      ],
-      "color": "Gray",
-      "visible": true
-    }, */
-
-//-------------------------Landmarks -- DarkGreen----------------------------
-// \n\uf5a6
- 
- /*   {
-      "uuid": " ",
-      "name": "Basalt Delta Overlook\n\uf5a6",
-      "dimensionId": 1,
-      "position": [
-        45,
-        46,
-        176
-      ],
-      "color": "DarkGreen",
-      "visible": true
-    } */
- 
+  "uuid": " ",
+  "name": "Ice Hut\n\uf5a6",
+  "dimensionId": 0,
+  "position": [
+    -267,
+    67,
+    644        
+  ],
+  "color": "DarkGreen",
+  "visible": true
+} 
   ]
 };
 
